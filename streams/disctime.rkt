d4fab325e590bf5c41b9c1031e25ad85fb2c1127
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-12-20 15:30:16 -0800

    Add disctime encoding

---
+++ streams/disctime.rkt
@@ -0,0 +1,61 @@
+#lang rosette/safe
+
+(provide (all-defined-out))
+
+(require (prefix-in r/ (only-in rosette/base/base map filter))
+ rosette/lib/angelic rosette/lib/match
+ (prefix-in l/ "../lang.rkt")
+ "../lib.rkt")
+
+
+; Data types
+
+(define noevent '())
+(define nevt '())
+
+(define noevent? null?)
+(define nevt? null?)
+
+; (struct event (stamp value) #:transparent)
+
+
+; Operators
+
+(define (map fn stream)
+  (r/map (lambda (x) (if (nevt? x) x (fn x))) stream)
+  )
+
+; (define (filter fn stream)
+;   (r/filter (lambda (x) (if (empty? x) x (fn x))) stream)
+; )
+
+(define (filter predicate stream)
+  (r/map (lambda (x) (if (or (nevt? x) (not (predicate x))) nevt x)) stream)
+  )
+
+
+; Sources
+
+(define (from-diagram diagramString)
+  (define interval 20)
+  (define characters (cdr (drop-right (string-split diagramString "") 1)))
+  (define (rec chars outstream)
+    (match chars
+      [(cons "-" xs) (rec xs (cons nevt outstream))]
+      [(cons x xs) (rec xs (cons x outstream))]
+      ['() outstream]
+      )
+    )
+  (reverse (rec characters '()))
+  )
+
+
+; Syntax
+
+; (define (interpret p)
+;   (match p
+;     [(l/map a b) (map a (interpret b))]
+;     [(l/filter a b) (filter a (interpret b))]
+;     [_ p]
+;     )
+;   )
---
+++ test/streams/disctime.test.rkt
@@ -0,0 +1,114 @@
+#lang rosette/safe
+
+(require rackunit rackunit/text-ui
+ (prefix-in s/ "../../streams/disctime.rkt")
+ (prefix-in l/ "../../lang.rkt"))
+
+(provide (all-defined-out))
+
+
+; Test data types
+
+(define (test-noevent)
+  (test-case
+    "test-noevent"
+    (check-equal? s/noevent '())
+    (check-true (s/noevent? '()))
+    (check-equal? s/nevt '())
+    (check-true (s/nevt? '()))
+    )
+  )
+
+
+; Test transformers
+
+(define (test-map)
+  (test-case
+    "test-map"
+    (check-equal?
+      (s/map add1 (list 0 0 0))
+      (list 1 1 1)
+      )
+    (check-equal?
+      (s/map add1 (list 0 s/noevent 0))
+      (list 1 s/noevent 1)
+      )
+    )
+  )
+
+(define (test-filter)
+  (test-case
+    "test-filter"
+    (check-equal?
+      (s/filter even? (list 0 1 2))
+      (list 0 s/noevent 2)
+      )
+    )
+  )
+
+
+; Test sources
+
+(define (test-from-diagram)
+  (test-case
+    "test-from-diagram"
+    (define outstream1 (s/from-diagram "1234"))
+    (define expected1
+      (list "1" "2" "3" "4"))
+    (check-equal? outstream1 expected1)
+    (define outstream2 (s/from-diagram "5--6"))
+    (define expected2 (list "5" s/noevent s/noevent "6"))
+    (check-equal? outstream2 expected2)
+    )
+  )
+
+
+; ; Test solver-aided programming
+
+; (current-bitwidth #f)
+; (define-symbolic x y integer?)
+
+; (define (test-solve)
+;   (test-case
+;     "test-solve"
+;     (define prog (l/map add1 (list (s/event x y))))
+;     (define sol (solve
+;       (assert (equal? (s/interpret prog) (list (s/event 0 2))))))
+;     (check-true (sat? sol))
+;     (check-equal? (evaluate (list x y) sol) (list 0 1))
+;     )
+;   )
+
+; (define-symbolic b boolean?)
+
+; (define (test-angexe)
+;   (test-case
+;     "test-angexe"
+;     (define prog
+;       (if b
+;         (l/map add1 (list (s/event x y)))
+;         (l/filter odd? (list (s/event x y)))
+;         )
+;       )
+;     (define sol (solve
+;       (assert (equal? (s/interpret prog) (list (s/event 0 2))))))
+;     (check-true (sat? sol))
+;     (check-equal? (evaluate (list b x y) sol) (list #t 0 1))
+;     )
+;   )
+
+
+; Main
+
+(module+ test
+  (define/provide-test-suite disctime-tests
+    (test-noevent)
+    (test-map)
+    (test-filter)
+    ; (test-ap)
+    (test-from-diagram)
+    ; (test-solve)
+    ; (test-angexe)
+    )
+  (run-tests disctime-tests)
+  )
